b20f7071fdebe2138feec9a4499da77c29691c7a
LuisPhDS <luis.phillipe358@gmail.com>
2023-10-16 16:10:00 -0300

    Dockerfile deploy

---
+++ src/test/java/br/com/luisphillipe/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luisphillipe.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
